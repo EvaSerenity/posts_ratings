5493f84f1baa2a3b5d080c0ff73ba8a166bd282f
evachan <etsunya@gmail.com>
2017-12-18 14:57:38 +0300

    added seeds service

--- app/services/sample_data_service.rb
+++ app/services/sample_data_service.rb
@@ -1,7 +1,26 @@
-module SampleDataService
+class SampleDataService
 
-  def perform
+  def self.perform
+    ips = []
+    50.times { ips << Faker::Internet.ip_v4_address }
+    users = []
+    100.times { users << UseCases::CreateUser.new(Faker::Name.name).perform.user}
+    posts = []
+    2000.times do
+      posts << UseCases::CreatePost
+                   .new(Faker::Lorem.word,
+                        Faker::Lorem.paragraph,
+                        users.sample,
+                        ips.sample)
+                   .perform
+                   .post
+    end
 
+    3000.times do
+      UseCases::CreateRating
+          .new(posts.sample.id,
+               Faker::Number.between(1, 5))
+          .perform
+    end
   end
-
 end
---
+++ db/seeds.rb
@@ -0,0 +1,1 @@
+SampleDataService.perform
